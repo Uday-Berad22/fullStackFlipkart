f401514c1eca3556d15c3acae348ffd08bdec185
beradug22 <beradug22@gmail.com>
2023-12-21 23:36:24 +0530

    final

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
-
+const port = process.env.PORT || 8000;
 //crearting app
 const app = express();
 // storing required paths
